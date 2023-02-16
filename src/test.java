94385a5b866bebed728eb03d70e3620a99570d5b
ssan <y18409190999@163.com>
2023-02-16 12:33:07 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,15 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("\\\"hello\\\"");
+        double x = 2.0;
+//        int y =4;
+//        x/=++y;
+//        System.out.println(x);
+        int a = 1;
+        float b = 2.5f;
+        a = (int) b;
+        System.out.println(a);
+
+
+    }
+}
